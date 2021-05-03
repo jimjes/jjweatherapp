d275967a8e3dedfd0defbdd9aad360970e8edb1e
Glitch (substantial-plucky-guide) <none>
2021-05-03 15:40:22 +0000

    ⛩📫 Checkpoint ./app.css:30703421/189 ./app.js:30703421/391

--- app.js
+++ app.js
@@ -20,17 +20,15 @@ form.addEventListener("submit", async e => {
   console.log(result);
   const template = `
   
-  <div class="card">
+  <div class="card" id="grad1">
   <h2>${result.name}, ${result.sys.country}</h2>
-  
   <p><img src="https://openweathermap.org/img/wn/${result.weather[0].icon}@4x.png"></p>
-
   <b>Temperature: ${result.main.temp}<sup>°C</sup></b><br />
   <b>Max Temperature: ${result.main.temp_max}<sup>°C</sup></b><br />
   <b>Min Temperature: ${result.main.temp_min}<sup>°C</sup></b><br />
   <p><b>Humidity: ${result.main.humidity}</b></p>
 
-</div>
+  </div>
   `
   placeholder.innerHTML = template;
   
